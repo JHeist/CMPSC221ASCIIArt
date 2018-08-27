9bff7c8b1cbe846363ab92f14d71a64613eacb4d
JHeist <42556389+JHeist@users.noreply.github.com>
2018-08-27 15:06:24 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,28 @@
+/**
+ * ASCIIArt
+ * @author Jeremy Heist CMPSC 221
+ */
 public class Main 
 {
   
    public static void main(String[] args) 
    {
-      System.out.println("  *  ");
-      System.out.println(" *** ");
-      System.out.println("*****");
+      System.out.println("            ***    ***************    ***       ***");
+      System.out.println("            ***    ***************    ***       ***");
+      System.out.println("            ***    ***   ***   ***    ***       ***");
+      System.out.println("            ***    ***   ***   ***    ***       ***");
+      System.out.println("            ***    ***   ***   ***    ***       ***");
+      System.out.println("            ***    ***   ***   ***    ***       ***");
+      System.out.println("            ***    ***   ***   ***    ***       ***");
+      System.out.println("            ***    ***   ***   ***    *************");
+      System.out.println("            ***    ***   ***   ***    *************");
+      System.out.println("            ***    ***   ***   ***    ***       ***");
+      System.out.println("            ***    ***   ***   ***    ***       ***");
+      System.out.println("            ***    ***   ***   ***    ***       ***");
+      System.out.println("***         ***    ***   ***   ***    ***       ***");
+      System.out.println("***         ***    ***   ***   ***    ***       ***");
+      System.out.println("***************    ***   ***   ***    ***       ***");
+      System.out.println("***************    ***   ***   ***    ***       ***");
   }
   
 }
